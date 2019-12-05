de2ae17e52c814f9dd276665254ee217e98b8870
Nandhini Ravisankar <nandhini.ravisankar@accenture.com>
2019-12-05 12:34:47 +0530

    added

--- src/test/java/testdemo/Online_Shopping.java
+++ src/test/java/testdemo/Online_Shopping.java
@@ -1,21 +1,33 @@
 package testdemo;
 
+import org.testng.annotations.Test;
+import org.testng.annotations.BeforeMethod;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.Test;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
 
-public class Online_Shopping {
-	@Test
-	public void login()
-	{
-		System.setProperty("webdriver.chrome.driver","C:\\Users\\A08019DIRP_C2C.02.02\\Downloads\\chromedriver_win32.zip");
-		WebDriver drivers=new ChromeDriver();
-		drivers.get("http://newtours.demoaut.com/");
-		drivers.findElement(By.name("userName")).sendKeys("mercury");
-		  drivers.findElement(By.name("password")).sendKeys("mercury");
-		  drivers.findElement(By.xpath("//input[@value='Login']")).click();
-		
-	}
+public class Online_Shopping{
+	WebDriver driver;
+  @Test
+  public void f() {
+	  driver.findElement(By.name("userName")).sendKeys("mercury");
+	  driver.findElement(By.name("password")).sendKeys("mercury");
+	  driver.findElement(By.xpath("//input[@value='Login']")).click();
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  //launch browser and application
+	  System.setProperty("webdriver.chrome.driver","C:\\Users\\A08019DIRP_C2C.02.02\\Downloads\\chromedriver_win32 (1)\\chromedriver.exe");
+	  driver=new ChromeDriver();
+	  driver.get("http://newtours.demoaut.com");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+	  Assert.assertTrue(driver.getTitle().contains("Flight"));
+	  driver.close();
+  }
 
 }
